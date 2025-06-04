4872842ae24fdff31c1fe898615d97b251460d4d
iamsupreetha <supreethahc387@gmail.com>
2025-06-04 10:25:44 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -4,8 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
+
     @Test
     public void testAdd() {
         assertEquals(5, App.add(2, 3));
     }
 }
+
